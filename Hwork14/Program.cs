f7bc2a20e00be14868f4800c9e86ce6955874300
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-10-28 23:02:11 +0300

    Добавьте файлы проекта.

---
+++ Hwork14/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.ShowInfo();
+            Dog dog = new Dog();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+
+            public Animal(string name = "питомец")
+            {
+                Name = name;
+            }
+
+            public abstract void Say();
+
+            public void ShowInfo()
+            {
+                Console.Write($"{Name}: ");
+                Say();
+            }
+        }
+        class Cat : Animal
+        {
+            string name;
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = "Кошка";
+                }
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine("Мяу мяу");
+                return;
+            }
+
+        }
+        class Dog : Animal
+        {
+            string name;
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = "Собака";
+                }
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine("Гав гав");
+                return;
+            }
+        }
+    }
+}
